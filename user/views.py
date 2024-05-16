98111186e13a7eb032b365591226cb9ecea2bfd0
nicat85a1 <nicat85a1@gmail.com>
2024-05-16 11:57:47 -0400

    first commit

--- user/views.py
+++ user/views.py
@@ -5,19 +5,23 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 # Create your views here.
-
+    
 def register_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
+        else:
+            messages.error(request, "bu kullanici artik mevcut")
+            form = RegisterForm()
     else:
         form = RegisterForm()
-        context = {
-            'form': form
-        }
-        return render(request, "user/register.html",context)
+    context = {
+        'form': form
+    }
+    return render(request, "user/register.html", context)
+
     
 def login_view(request):
     if request.method == 'POST':
